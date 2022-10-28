86fe55a0a15490ac6c06fcd2ebcad57abf2a3e76
Bryan Antonis <bryan-antonis@hotmail.com>
2022-10-28 15:30:20 +0200

    added react and fastapi bases

---
+++ tools/conda-env.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+PROJECT_NAME="lemp-tm"
+PYTHON_VERSION="3.10"
+
+echo "Setting up conda environment for $PROJECT_NAME (python $PYTHON_VERSION)"
+
+if command -v conda
+then
+    conda create --name $PROJECT_NAME python=$PYTHON_VERSION -y
+    conda install --name $PROJECT_NAME -c conda-forge fastapi -y
+    exit 0
+else
+    echo "Conda is not installed!"
+    exit 1
+fi
